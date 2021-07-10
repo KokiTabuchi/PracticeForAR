a54ffcbc8d1bbd069007856e5b1cba4b1520c6d3
nozomishira <71810673+nozomishira@users.noreply.github.com>
2021-07-10 19:46:36 +0900

    Spawn Enemy
    Script about spawning from bee comb.
    and I tried to movement of bee.

--- Assets/Scripts/shiraishi_Scripts/Honeycomb_Emerge.cs
+++ Assets/Scripts/shiraishi_Scripts/Honeycomb_Emerge.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using DG.Tweening;
+using UnityEngine.AI;//NavMeshagentを使うために記述する
 
 public class Honeycomb_Emerge : MonoBehaviour
 {
+    
+    public GameObject Player;
     //蜂の巣のゲームオブジェクト
     public GameObject comb;
 
@@ -23,15 +26,48 @@ public class Honeycomb_Emerge : MonoBehaviour
     //鉢の巣を表示させるかどうか
      private bool isComb = false;
 
+    //出現させる蜂のオブジェクト 
+    [SerializeField]
+    private GameObject[] bee = new GameObject[10];//とりあえず１０体出てくるように設定
+    private GameObject[] spawnedEnemy = new GameObject[10];//スポーンされたときに格納されるもの
+
+    //敵の生成の時間とインターバル
+    float timer = 0.0f;
+    float enemyInterval = 15.0f;
+
+
+    Vector3[] wayPoints = new Vector3[3];//徘徊するポイントの座標を代入するVector3型の変数を配列で作る
+    private int currentRoot;//現在目指すポイントを代入する変数
+    private int Mode;//敵の行動パターンを分けるための変数
+    //public Transform player;//プレイヤーの位置を取得するためのTransform型の変数
+    //public Transform enemypos;//敵の位置を取得するためのTransform型の変数
+    private NavMeshAgent agent;//NavMeshAgentの情報を取得するためのNavmeshagent型の変数
+
+
+    // オブジェクトが停止するターゲットオブジェクトとの距離を格納する変数
+    public float stopDistance = 10.0f;
+    // オブジェクトがターゲットに向かって移動を開始する距離を格納する変数
+    public float moveDistance = 10.0f;
+    //敵の速さ
+    float moveSpeed = 1.0f;
+
     public void Start()
     {
+        //Player = GameObject.Find("Player");
+        agent = GetComponent<NavMeshAgent>();//NavMeshAgentの情報をagentに代入
         comb.SetActive(false);//蜂の巣を非表示にする
         setHonneyComb();//蜂の巣オブジェクトを表示する場所をランダムで決定
-        
+
+        //敵の移動目的ポイント３つ設定
+        wayPoints[0] = new Vector3(Player.transform.position.x + 3.0f, Player.transform.position.y, Player.transform.position.z);
+        wayPoints[1] = new Vector3(Player.transform.position.x, Player.transform.position.y+3.0f, Player.transform.position.z);
+        wayPoints[2] = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z+3.0f);
     }
     public void Update()
     {
-       
+        timer += Time.deltaTime;
+
+      
         //getkilled_Enemyメソッドで倒したエネミーの数を引き取り、巣が発生する上限と比較する
         if ((killed_enemy_num.getKilled_EnemyNum()) >= killedEnemy_upper_limit)
         {
@@ -39,6 +75,13 @@ public class Honeycomb_Emerge : MonoBehaviour
             {
                 Emerge_Comb();
             }
+            //一定時間で敵を生成
+            if (timer > enemyInterval)
+            {
+                timer = 0.0f;
+                emergeBee();//蜂を発生させるメソッドの呼び出し
+            }
+           
         }
 
     }
@@ -62,5 +105,95 @@ public class Honeycomb_Emerge : MonoBehaviour
         honeycomb_pos_y = Random.Range(-10.0f, 10.0f);
         honeycomb_pos_z = Random.Range(6.0f, 15.0f);
         comb.transform.position = new Vector3( honeycomb_pos_x, honeycomb_pos_y, honeycomb_pos_z);
+    }
+
+    //巣から蜂を発生させるスクリプト
+    public void emergeBee()
+    {
+        //蜂を生成する(数は配列文だけで、場所は蜂の巣の外側から
+        for(int i = 0; i< bee.Length; i++)
+        {
+            if (!spawnedEnemy[i])
+            {
+                spawnedEnemy[i] = Instantiate(bee[i],
+                        new Vector3(honeycomb_pos_x + comb.transform.localScale.x / 2,
+                                    honeycomb_pos_y + comb.transform.localScale.y / 2,
+                                    honeycomb_pos_z + comb.transform.localScale.z / 2
+                                    ),
+                         Quaternion.LookRotation(Player.transform.position));
+            }
+        }
+
+        for(int i = 0; i < spawnedEnemy.Length; i++)
+        {
+            if(i%2 == 0)
+            {
+                enemyMoveRandom(i);
+            }
+            else
+            {
+                //enemyMoveDirect();
+                enemyMoveRandom(i);
+            }
+        }
+    }
+
+    //敵を徘徊するように動かすスクリプト
+    public void enemyMoveRandom(int enemy_num)
+    {
+        Vector3 pos = wayPoints[currentRoot];//Vector3型のposに現在の目的地の座標を代入
+        float distance = Vector3.Distance(spawnedEnemy[enemy_num].transform.position, Player.transform.position);//敵とプレイヤーの距離を求める
+
+        if (distance > 5)
+        {//もしプレイヤーと敵の距離が5以上なら
+            Mode = 0;//Modeを0にする
+        }
+
+        if (distance < 5)
+        {//もしプレイヤーと敵の距離が5以下なら
+            Mode = 1;//Modeを1にする
+        }
+
+        switch (Mode)
+        {//Modeの切り替えは
+
+            case 0://case0の場合
+
+                if (Vector3.Distance(transform.position, pos) < 1f)
+                {//もし敵の位置と現在の目的地との距離が1以下なら
+                    currentRoot += 1;//currentRootを+1する
+                    if (currentRoot > wayPoints.Length - 1)
+                    {//もしcurrentRootがwayPointsの要素数-1より大きいなら
+                        currentRoot = 0;//currentRootを0にする
+                    }
+                }
+                GetComponent<NavMeshAgent>().SetDestination(pos);//NavMeshAgentの情報を取得し目的地をposにする
+                break;//switch文の各パターンの最後につける
+
+            case 1://case1の場合
+                agent.destination = Player.transform.position;//プレイヤーに向かって進む		
+                break;//switch文の各パターンの最後につける
+        }
+
+        //敵の移動目的ポイントの変更
+        float Targetpoint = Random.Range(-3.0f, 3.0f);
+        wayPoints[0] = new Vector3(Player.transform.position.x + Targetpoint, Player.transform.position.y, Player.transform.position.z);
+        wayPoints[1] = new Vector3(Player.transform.position.x, Player.transform.position.y + Targetpoint, Player.transform.position.z);
+        wayPoints[2] = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z + Targetpoint);
+    }
+
+    public void enemyMoveDirect(int enemy_num)
+    {
+        spawnedEnemy[enemy_num].transform.LookAt(Player.transform.position);
+        // 変数 distance を作成してオブジェクトの位置とターゲットオブジェクトの距離を格納
+        float dist = Vector3.Distance(spawnedEnemy[enemy_num].transform.position, Player.transform.position);
+        // オブジェクトとターゲットオブジェクトの距離判定
+        // 変数 distance（ターゲットオブジェクトとオブジェクトの距離）が変数 moveDistance の値より小さければ
+        // さらに変数 distance が変数 stopDistance の値よりも大きい場合
+        if (dist < moveDistance && dist > stopDistance)
+        {
+            // 変数 moveSpeed を乗算した速度でオブジェクトを前方向に移動する
+            transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
+        }
     }
 }
