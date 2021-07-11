2aa2d6d5df61fd25dc05f9a9620b2efb4182cdd3
nozomishira <71810673+nozomishira@users.noreply.github.com>
2021-07-11 12:54:12 +0900

    つまった

---
+++ Assets/Scripts/shiraishi_Scripts/Enemy_Movement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+   //敵を徘徊するように動かすスクリプト
+   public void enemyMoveRandom(int enemy_num)
+   {
+       Debug.Log("移動");
+       //wayPoints[0] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y+ Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+       Vector3 pos = wayPoints2[enemy_num][currentRoot];//Vector3型のposに現在の目的地の座標を代入
+       float distance = Vector3.Distance(spawnedEnemy[enemy_num].transform.position, Player.transform.position);//敵とプレイヤーの距離を求める
+
+       if (distance > 3)
+       {//もしプレイヤーと敵の距離が5以上なら
+           Mode = 0;//Modeを0にする
+       }
+
+       if (distance < 3)
+       {//もしプレイヤーと敵の距離が5以下なら
+           Mode = 1;//Modeを1にする
+       }
+
+       switch (Mode)
+       {//Modeの切り替えは
+
+           case 0://case0の場合
+
+               if (Vector3.Distance(spawnedEnemy[enemy_num].transform.position, pos) < 1f)
+               {//もし敵の位置と現在の目的地との距離が1以下なら
+                   currentRoot += 1;//currentRootを+1する
+                   if (currentRoot > wayPoints2[enemy_num].Length - 1)
+                   {//もしcurrentRootがwayPointsの要素数-1より大きいなら
+                       currentRoot = 0;//currentRootを0にする
+                   }
+               }
+               //次の目的場所を向く
+               spawnedEnemy[enemy_num].transform.LookAt(wayPoints2[enemy_num][currentRoot]);
+               spawnedEnemy[enemy_num].transform.position = spawnedEnemy[enemy_num].transform.position + spawnedEnemy[enemy_num].transform.forward * moveSpeed * Time.deltaTime;//GetComponent<NavMeshAgent>().SetDestination(pos);//NavMeshAgentの情報を取得し目的地をposにする
+               break;//switch文の各パターンの最後につける
+
+           case 1://case1の場合
+               enemyMoveDirect(enemy_num);//プレイヤーに向かって進む		
+               break;//switch文の各パターンの最後につける
+       }
+
+       //敵の移動目的ポイントの変更
+       //float Targetpoint = Random.Range(-3.0f, 3.0f);
+     //  wayPoints[0] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+      // wayPoints[1] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+      // wayPoints[2] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+   }
+
+   public void enemyMoveDirect(int enemy_num)
+   {
+       //敵がプレイヤーのほうを向く
+       spawnedEnemy[enemy_num].transform.LookAt(Player.transform.position);
+
+       // 変数 moveSpeed を乗算した速度でオブジェクトを前方向に移動する
+       spawnedEnemy[enemy_num].transform.position = spawnedEnemy[enemy_num].transform.position + spawnedEnemy[enemy_num].transform.forward * moveSpeed * Time.deltaTime;
+   }
+   */
+}
--- Assets/Scripts/shiraishi_Scripts/Honeycomb_Emerge.cs
+++ Assets/Scripts/shiraishi_Scripts/Honeycomb_Emerge.cs
@@ -14,7 +14,7 @@ public class Honeycomb_Emerge : MonoBehaviour
 
     //蜂の殺した数 Killed_Enemy_Countクラスのインスタンス変数
     Killed_Enemy_Count killed_enemy_num = new Killed_Enemy_Count();
-    GameSceneManager killed_Manager  = new GameSceneManager();
+    GameSceneManager killed_Manager = new GameSceneManager();
 
     //蜂を殺したら巣が出現するための一定条件
     const int killedEnemy_upper_limit = 0;
@@ -25,7 +25,7 @@ public class Honeycomb_Emerge : MonoBehaviour
     float honeycomb_pos_z = 0.0f;
 
     //鉢の巣を表示させるかどうか
-     private bool isComb = false;
+    private bool isComb = false;
 
     //出現させる蜂のオブジェクト 
     [SerializeField]
@@ -34,10 +34,14 @@ public class Honeycomb_Emerge : MonoBehaviour
 
     //敵の生成の時間とインターバル
     float timer = 0.0f;
-    float enemyInterval = 5.0f;
+    float enemyInterval = 3.0f;
 
+  
+     //IDictionary<int, Vector3> map = new Dictionary<int, Vector3>();
+    //List<int> list = new List<int>(10);
 
     Vector3[] wayPoints = new Vector3[3];//徘徊するポイントの座標を代入するVector3型の変数を配列で作る
+    public Vector3[][] wayPoints2 = new Vector3[10][];//蜂それぞれに対して徘徊するポイントの座標を代入するVector3型の変数を配列で作る
     private int currentRoot=0;//現在目指すポイントを代入する変数
     private int Mode;//敵の行動パターンを分けるための変数
     //public Transform player;//プレイヤーの位置を取得するためのTransform型の変数
@@ -45,19 +49,30 @@ public class Honeycomb_Emerge : MonoBehaviour
     //private NavMeshAgent agent;//NavMeshAgentの情報を取得するためのNavmeshagent型の変数
 
     //敵の速さ
-    float moveSpeed = 1.0f;
+    float moveSpeed = 0.4f;
 
     public void Start()
     {
+        
         //Player = GameObject.Find("Player");
-       // agent = GetComponent<NavMeshAgent>();//NavMeshAgentの情報をagentに代入
+        // agent = GetComponent<NavMeshAgent>();//NavMeshAgentの情報をagentに代入
         comb.SetActive(false);//蜂の巣を非表示にする
         setHonneyComb();//蜂の巣オブジェクトを表示する場所をランダムで決定
 
-        //敵の移動目的ポイント３つ設定
-        wayPoints[0] = new Vector3(Player.transform.position.x + 7.0f, Player.transform.position.y, Player.transform.position.z);
-        wayPoints[1] = new Vector3(Player.transform.position.x, Player.transform.position.y+7.0f, Player.transform.position.z);
-        wayPoints[2] = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z+7.0f);
+        /*   wayPoints[0] = new Vector3(Player.transform.position.x + 7.0f, Player.transform.position.y, Player.transform.position.z);
+           wayPoints[1] = new Vector3(Player.transform.position.x, Player.transform.position.y+7.0f, Player.transform.position.z);
+           wayPoints[2] = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z+7.0f);*/
+      /*  wayPoints[0] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+        wayPoints[1] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+        wayPoints[2] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+      */
+        for(int i = 0; i < wayPoints2.Length; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                wayPoints2[i][j] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z + Random.Range(-6.0f, 6.0f));
+            }
+        }
     }
     public void Update()
     {
@@ -76,7 +91,7 @@ public class Honeycomb_Emerge : MonoBehaviour
             //{
                 timer = 0.0f;
                 emergeBee();//蜂を発生させるメソッドの呼び出し
-            //}
+           // }
            
         }
 
@@ -117,69 +132,16 @@ public class Honeycomb_Emerge : MonoBehaviour
                                     honeycomb_pos_z + comb.transform.localScale.z / 2
                                     ),
                          Quaternion.LookRotation(Player.transform.position));
+                spawnedEnemy[i].transform.LookAt(Player.transform.position);
             }
         }
 
         for(int i = 0; i < spawnedEnemy.Length; i++)
         {
-                enemyMoveRandom(i);
+                //enemyMoveRandom(i);
         }
     }
 
-    //敵を徘徊するように動かすスクリプト
-    public void enemyMoveRandom(int enemy_num)
-    {
-        Debug.Log("移動");
-        Vector3 pos = wayPoints[currentRoot];//Vector3型のposに現在の目的地の座標を代入
-        float distance = Vector3.Distance(spawnedEnemy[enemy_num].transform.position, Player.transform.position);//敵とプレイヤーの距離を求める
-
-        if (distance > 3)
-        {//もしプレイヤーと敵の距離が5以上なら
-            Mode = 0;//Modeを0にする
-        }
-
-        if (distance < 3)
-        {//もしプレイヤーと敵の距離が5以下なら
-            Mode = 1;//Modeを1にする
-        }
-
-        switch (Mode)
-        {//Modeの切り替えは
-
-            case 0://case0の場合
-
-                if (Vector3.Distance(spawnedEnemy[enemy_num].transform.position, pos) < 1f)
-                {//もし敵の位置と現在の目的地との距離が1以下なら
-                    currentRoot += 1;//currentRootを+1する
-                    if (currentRoot > wayPoints.Length - 1)
-                    {//もしcurrentRootがwayPointsの要素数-1より大きいなら
-                        currentRoot = 0;//currentRootを0にする
-                    }
-                }
-                //次の目的場所を向く
-                spawnedEnemy[enemy_num].transform.LookAt(wayPoints[currentRoot]);
-                spawnedEnemy[enemy_num].transform.position = spawnedEnemy[enemy_num].transform.position + spawnedEnemy[enemy_num].transform.forward * moveSpeed * Time.deltaTime;//GetComponent<NavMeshAgent>().SetDestination(pos);//NavMeshAgentの情報を取得し目的地をposにする
-                break;//switch文の各パターンの最後につける
-
-            case 1://case1の場合
-                enemyMoveDirect(enemy_num);//プレイヤーに向かって進む		
-                break;//switch文の各パターンの最後につける
-        }
-
-        //敵の移動目的ポイントの変更
-        //float Targetpoint = Random.Range(-3.0f, 3.0f);
-        wayPoints[0] = new Vector3(Player.transform.position.x + Random.Range(-6.0f, 6.0f), Player.transform.position.y, Player.transform.position.z);
-        wayPoints[1] = new Vector3(Player.transform.position.x, Player.transform.position.y + Random.Range(-6.0f, 6.0f), Player.transform.position.z);
-        wayPoints[2] = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z + Random.Range(-6.0f, 6.0f));
-    }
-
-    public void enemyMoveDirect(int enemy_num)
-    {
-        //敵がプレイヤーのほうを向く
-        spawnedEnemy[enemy_num].transform.LookAt(Player.transform.position);
-
-        // 変数 moveSpeed を乗算した速度でオブジェクトを前方向に移動する
-        spawnedEnemy[enemy_num].transform.position = spawnedEnemy[enemy_num].transform.position + spawnedEnemy[enemy_num].transform.forward * moveSpeed * Time.deltaTime;
-    }
+   
 }
 
